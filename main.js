fd0129741f37c58f5423004c76b1c10eb547a86f
Muh1isa <lisaa27.a6@gmail.com>
2023-09-25 06:36:13 +0500

    switch&case problems answers

---
+++ main.js
@@ -0,0 +1,198 @@
+// 1
+// let day = Number(prompt(''));
+
+// switch (day) {
+//     case 1:
+//         alert('Monday');
+//         break;
+//     case 2:
+//         alert('Tuesday');
+//         break
+//     case 3:
+//         alert('Wednesday');
+//         break
+//     case 4: 
+//         alert('Thursday');
+//         break
+//     case 5:
+//         alert('Friday');
+//         break
+//     case 6:
+//         alert('Saturday');
+//         break
+//     case 7:
+//         alert('Sunday');
+//         break
+//     default:
+//         alert('Not defined');
+//         break;
+// }
+
+
+
+// 2
+// let mark = Number(prompt(''))
+
+// switch (mark) {
+//     case 1:
+//         alert('Yomon')
+//         break;
+//     case 2:
+//         alert('Qoniqarsiz')
+//         break;
+//     case 3:
+//         alert('Qoniqarli')
+//         break;
+//     case 4:
+//         alert('Yaxshi')
+//         break;
+//     case 5:
+//         alert('A`lo')
+//         break;
+//     default:
+//         break;
+// }
+
+
+
+// 3
+// let month = Number(prompt(''))
+
+// switch (month) {
+//     case 1:
+//         alert('1 - winter')
+//         break;
+//     case 2:
+//         alert('2 - winter')
+//         break
+//     case 3:
+//         alert('3 - spring')
+//         break
+//     case 4:
+//         alert('4 - spring')
+//         break
+//     case 5:
+//         alert('5 - spring')
+//         break
+//     case 6:
+//         alert('6 - summer')
+//         break
+//     case 7:
+//         alert('7 - summer')
+//         break
+//     case 8:
+//         alert('8 - summer')
+//         break
+//     case 9:
+//         alert('9 - autumn')
+//         break
+//     case 10:
+//         alert('10 - autumn')
+//         break
+//     case 11: 
+//         alert('11 - autumn')
+//         break
+//     case 12:
+//         alert('12 - winter')
+//         break
+//     default:
+//         alert(false)
+//         break;
+// }
+
+
+
+// 4
+// let month = Number(prompt(''))
+// switch (month) {
+//     case 1:
+//         alert('1 - 31')        
+//         break;
+//     case 2:
+//         alert('2 - 28')
+//         break
+//     case 3:
+//         alert('3 - 31')
+//         break
+//     case 4:
+//         alert('4 - 30')
+//         break
+//     case 5:
+//         alert('5 - 31')
+//         break
+//     case 6:
+//         alert('6 - 30')
+//         break
+//     case 7:
+//         alert('7 - 31')
+//         break
+//     case 8:
+//         alert('8 - 31')
+//         break
+//     case 9:
+//         alert('9 - 30')
+//         break
+//     case 10:
+//         alert('10 - 31')
+//         break
+//     case 11:
+//         alert('11 - 30')
+//         break
+//     case 12:
+//         alert('12 - 31')
+//         break
+//     default:
+//         alert(false)
+//         break;
+// }
+
+
+
+// 5
+// let a = Number(prompt('a sonni kiriting: '))
+// let b = Number(prompt('b sonni kiriting: '))
+// let c = prompt('Matematik amal kiriting(+,-,*,/): ')
+
+// switch (c) {
+//     case '+':
+//         alert(a + b)
+//         break;
+//     case '-':
+//         alert(a - b)
+//         break;
+//     case '*':
+//         alert(a * b)
+//         break;
+//     case '/':
+//         alert(a / b)
+//         break;
+//     default:
+//         alert(false)
+//         break;
+// }
+
+
+
+// 6
+// let mark = Number(prompt(''))
+
+// switch (mark) {
+//     case 1:
+//         alert('Not give ') 
+//         break;
+//     case 2:
+//         alert('Not give') 
+//         break;
+//     case 3:
+//         alert('3 - 320') 
+//         break;
+//     case 4:
+//         alert('4 - 520') 
+//         break;
+//     case 5:
+//         alert('5 - 620') 
+//         break;
+//     default:
+//         alert(false)
+//         break;
+// }
